96fa48c47dce7f41df92b24111b9e547ea382656
driendro <jorge.ronconi@gmail.com>
2019-01-21 16:43:48 -0300

    agregando los modelos al admin site

--- src/apps/apuntes/admin.py
+++ src/apps/apuntes/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+from django.contrib				import admin
 
 # Register your models here.
+
+
+from apps.apuntes.models		import (
+										Materia,
+										Carrera,
+										Comision,
+										Docente,
+										Tipo,
+										Apunte,
+										)
+
+admin.site.register(Materia)
+admin.site.register(Carrera)
+admin.site.register(Comision)
+admin.site.register(Docente)
+admin.site.register(Tipo)
+admin.site.register(Apunte)
