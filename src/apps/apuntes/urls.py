16c0433541a2de72f8fa2441f4e93b052c725943
Driendro <jorge.ronconi@gmail.com>
2019-01-22 15:03:12 -0300

    Se acomodaron las urls

--- src/apps/apuntes/urls.py
+++ src/apps/apuntes/urls.py
@@ -1,3 +1,6 @@
+
+from django.urls import path
+
 urlpatterns = [
-	
+    path('apunte/', include('apps.apuntes.urls'))
 ]
--- src/fotoapp/urls.py
+++ src/fotoapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apunte/', include('apps.apuntes.urls'))
+    path('', include('apps.apuntes.urls'))
 
 ]
